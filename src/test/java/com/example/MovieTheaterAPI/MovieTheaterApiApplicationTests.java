1782c3d3df73f74c9d39a633486a26d8ed6fe29c
thuyluu <thuyluu9595@gmail.com>
2023-09-18 11:05:33 -0700

    initial commit

---
+++ src/test/java/com/example/MovieTheaterAPI/MovieTheaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieTheaterAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
