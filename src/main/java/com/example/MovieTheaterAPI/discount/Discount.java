74e77760498eb1f5bb6b371c8b00db422da261e6
Nguyen Duong <nguyen.duong@smoothstack.com>
2023-09-30 20:37:16 -0700

    Fix minor bug

--- src/main/java/com/example/MovieTheaterAPI/discount/Discount.java
+++ src/main/java/com/example/MovieTheaterAPI/discount/Discount.java
@@ -1,28 +1,29 @@
-//package com.example.MovieTheaterAPI.discount;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//@Setter
-//@Getter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//
-//@Entity
-//@Table(name = "discount")
-//public class Discount {
-//
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long id;
-//
-//  @Column(name = "title")
-//  private String title;
-//
-//  @Column(name = "before_6pm")
-//  private float isHappyHourBefore6PM;
-//
-//  @Column(name = "tuesday_discount")
-//  private float isTuesdayDiscount;
-//
-//}
+package com.example.MovieTheaterAPI.discount;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "discount")
+public class Discount {
+
+  @Id
+  @Enumerated(EnumType.STRING)
+  private DiscountType id;
+
+  @Column(name = "percent_discount")
+  @Min(value = 0, message = "Percentage discount cannot be less than 0")
+  @Max(value = 100, message = "Percentage discount cannot be greater than 100")
+  private float percentDiscount;
+}
+enum DiscountType {
+    TuedaySpecial,
+    Before6PM
+}
---
+++ src/main/java/com/example/MovieTheaterAPI/discount/dto/DiscountUpdateDTO.java
@@ -0,0 +1,8 @@
+package com.example.MovieTheaterAPI.discount.dto;
+
+import lombok.Data;
+
+@Data
+public class DiscountUpdateDTO {
+    private float percent;
+}
