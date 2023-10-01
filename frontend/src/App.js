7064e7f42ea82119e52dcaf9c36c9269006495f4
Chau Doan <chau.doan@smoothstack.com>
2023-09-30 19:34:26 -0700

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>THC Theater</a>
+      </header>
+      <main>
+        <h1>List Of Movies</h1>
+      </main>
+    </div>
+   
+  );
+}
+
+
