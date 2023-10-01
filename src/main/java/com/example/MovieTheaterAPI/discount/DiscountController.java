52943e93cba3588090a02cd53481e088e3a3dfbf
Nguyen Duong <nguyen.duong@smoothstack.com>
2023-09-30 20:37:16 -0700

    Fix minor bug

--- src/main/java/com/example/MovieTheaterAPI/discount/DiscountController.java
+++ src/main/java/com/example/MovieTheaterAPI/discount/DiscountController.java
@@ -1,33 +1,32 @@
-//package com.example.MovieTheaterAPI.discount;
-//
-//import java.util.List;
-//
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/discount")
-//public class DiscountController {
-//
-//  DiscountService discountService;
-//
-//  @GetMapping("/")
-//  public List<Discount> getAllDiscount(){
-//    return discountService.getAllDiscount();
-//  }
-//
-//  @GetMapping("/{id}")
-//  public Discount getDiscountById(@PathVariable Long id) {
-//    return discountService.getDiscountById(id);
-//  }
-//
-//  @GetMapping("/happy-hour/{isHappyHourBefore6PM}")
-//  public List<Discount> getDiscountByHappyHour(@PathVariable boolean isHappyHourBefore6PM) {
-//    return discountService.getDiscountByHappyHour(isHappyHourBefore6PM);
-//  }
-//
-//  @GetMapping("/tuesday/{isTuesdayDiscount}")
-//  public List<Discount> getDiscountByTuesday(@PathVariable boolean isTuesdayDiscount) {
-//    return discountService.getDiscountByTuesday(isTuesdayDiscount);
-//  }
-//
-//}
+package com.example.MovieTheaterAPI.discount;
+
+import java.util.List;
+
+import com.example.MovieTheaterAPI.discount.dto.DiscountUpdateDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/discount")
+@AllArgsConstructor
+public class DiscountController {
+    private final DiscountService discountService;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllDiscount(){
+        return ResponseEntity.ok(discountService.getAllDiscount());
+    }
+
+    @PutMapping("/tueday-special")
+    public ResponseEntity<?> updateTuedayDiscount(@RequestBody DiscountUpdateDTO discountUpdateDTO) {
+        return ResponseEntity.ok(discountService.updateTuedayDiscount(discountUpdateDTO.getPercent()));
+    }
+
+    @PutMapping("/before-6pm")
+    public ResponseEntity<?> updateBefore6PM(@RequestBody DiscountUpdateDTO discountUpdateDTO) {
+        return ResponseEntity.ok(discountService.updateBefore6PMDiscount(discountUpdateDTO.getPercent()));
+    }
+
+
+}
