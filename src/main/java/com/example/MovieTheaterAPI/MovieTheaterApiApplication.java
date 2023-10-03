c1d6fa5ecd29fb46576252f3d034fe88b2ec16b6
thuyluu <thuyluu9595@gmail.com>
2023-10-02 21:33:09 -0700

    Implemented aws s3 and storing image link

--- src/main/java/com/example/MovieTheaterAPI/MovieTheaterApiApplication.java
+++ src/main/java/com/example/MovieTheaterAPI/MovieTheaterApiApplication.java
@@ -1,13 +1,6 @@
 package com.example.MovieTheaterAPI;
-
-import com.example.MovieTheaterAPI.movie.s3.S3Service;
-import com.example.MovieTheaterAPI.movie.service.MovieService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.net.URL;
 
 @SpringBootApplication
 public class MovieTheaterApiApplication {
