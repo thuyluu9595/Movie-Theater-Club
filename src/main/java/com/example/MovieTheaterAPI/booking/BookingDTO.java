b10e4796a4abb9913a1188d23e9a244f193587e1
thuyluu <thuyluu9595@gmail.com>
2023-10-09 18:59:25 -0700

    Fixed bugs

--- src/main/java/com/example/MovieTheaterAPI/booking/BookingDTO.java
+++ src/main/java/com/example/MovieTheaterAPI/booking/BookingDTO.java
@@ -1,10 +1,16 @@
 package com.example.MovieTheaterAPI.booking;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class BookingDTO {
+    @JsonProperty("user_id")
     private Long userId;
+
+    @JsonProperty("show_time_id")
     private Long showTimeId;
+
+    @JsonProperty("seats")
     private int[] seats;
 }
