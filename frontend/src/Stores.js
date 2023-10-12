9cc08aff8edd4308dc8e5de293f5272c2cfa239a
Chau Doan <chau.doan@smoothstack.com>
2023-10-11 23:27:55 -0700

    show

--- frontend/src/Stores.js
+++ frontend/src/Stores.js
@@ -4,6 +4,7 @@ import { createContext, useReducer } from "react";
 export const Store = createContext();
 
 const initialState = {
+  
   userInfo: localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null,
--- frontend/src/screens/ShowTimeScreen.js
+++ frontend/src/screens/ShowTimeScreen.js
@@ -1,53 +1,82 @@
-import React, { useContext } from "react";
+import React, { useEffect, useReducer } from "react";
 import { Helmet } from "react-helmet";
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading: true};
+    case 'FETCH_SUCCESS':
+      return {...state, movies: action.payload, loading: false};
+    case 'FETCH_FAIL':
+      return {...state,loading:false, error: action.payload};
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  showtimes: [],
+  loading: false,
+  error: null,
+}
 
 export default function ShowTimeScreen() {
+  const [state, dispatch ] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const response = await axios.get('http://localhost:8080/api/showtimes');
+        dispatch({ type: 'FETCH_SUCCESS', payload: response.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: error });
+      }
+    }
+    fetchData();
+  }, []);
+
+  const {showtimes, loading, error } = state;
+
   return (
     <Container>
       <Helmet>
         <title>Show Time</title>
       </Helmet>
       <h1>Movie Showtimes</h1>
-      <table className="table table-bordered table-striped">
-        <thead className="thead">
-          <tr>
-              <th>Movie</th>
-              <th>Showtime</th>
-              <th>Location</th>
-              <th>Tickets</th>
-          </tr>
-        </thead>
-        <tbody className="tbody">
-          <tr>
-            <td>Movie 1</td>
-            <td>10:00 AM</td>
-            <td>Theater 1</td>
-            <td><a href="#" class="btn btn-primary">Buy Tickets</a></td>
-          </tr>
-          <tr>
-            <td>Movie 2</td>
-            <td>2:30 PM</td>
-            <td>Theater 2</td>
-            <td><a href="#" class="btn btn-primary">Buy Tickets</a></td>
-          </tr>
-          <tr>
-            <td>Movie 3</td>
-            <td>2:30 PM</td>
-            <td>Theater 3</td>
-            <td><a href="#" class="btn btn-primary">Buy Tickets</a></td>
-         </tr>
-         <tr>
-            <td>Movie 4</td>
-            <td>1:30 PM</td>
-            <td>Theater 4</td>
-            <td><a href="#" class="btn btn-primary">Buy Tickets</a></td>
-         </tr>
-
-        </tbody>
-      </table>
-      
+      { loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <table className="table table-bordered table-striped">
+          <thead className="thead">
+            <tr>
+                <th>Movie</th>
+                <th>Showtime</th>
+                <th>Location</th>
+                <th>Tickets</th>
+            </tr>
+          </thead>
+          <tbody className="tbody">
+            {showtimes.map((showtime, id) =>(
+              <tr key={id}>
+                <td>{showtime.movie}</td>
+                <td>{showtime.showtime}</td>
+                <td>{showtime.location}</td>
+                <td>
+                  <Link to='/'>Buy Tickets</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        )}
     </Container>
 
   )
