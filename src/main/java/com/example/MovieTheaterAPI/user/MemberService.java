e91f2d87a1faf378454f0f6e6fd94430ca0d4c38
Chau Doan <chaudoan1221@gmail.com>
2023-11-03 00:25:12 -0700

    fix member schema and fix payment

--- src/main/java/com/example/MovieTheaterAPI/user/MemberService.java
+++ src/main/java/com/example/MovieTheaterAPI/user/MemberService.java
@@ -2,9 +2,11 @@ package com.example.MovieTheaterAPI.user;
 
 import org.springframework.stereotype.Service;
 
+import com.example.MovieTheaterAPI.user.dto.UpgradeAccountDTO;
+
 public interface MemberService {
     Member createMember(User user);
-    Member upgradePremium(long id);
+    Member upgradePremium(long id, UpgradeAccountDTO dto);
     Member cancelPremium(long id);
     Member addPoint(long id, int point);
 }
--- src/main/java/com/example/MovieTheaterAPI/user/MemberServiceImpl.java
+++ src/main/java/com/example/MovieTheaterAPI/user/MemberServiceImpl.java
@@ -2,6 +2,11 @@ package com.example.MovieTheaterAPI.user;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.example.MovieTheaterAPI.payment.StripeService;
+import com.example.MovieTheaterAPI.user.dto.UpgradeAccountDTO;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.model.Subscription;
 
 import java.util.Optional;
 
@@ -9,18 +14,32 @@ import java.util.Optional;
 @AllArgsConstructor
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
+    private final StripeService stripeService;
+    
     @Override
     public Member createMember(User user) {
         Member member = new Member();
+        Customer customer = stripeService.createCustomer(user.getEmail(), user.getUsername());
         member.setUser(user);
+        member.setStripeCustomerId(customer.getId());
         return memberRepository.save(member);
     }
-    public Member upgradePremium(long id){
+
+
+
+    public Member upgradePremium(long id, UpgradeAccountDTO dto){
         Optional<Member> entity = memberRepository.findByUserId(id);
         Member member = unwrapMember(entity);
         if (member.getMembershipTier() == Tier.Premium) {
             throw new RuntimeException("Already upgraded to Premium account.");
         }
+        Subscription subscription;
+        try {
+            subscription = stripeService.createSubscription(dto, member);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        member.setSubscriptionId(subscription.getId());
         member.setMembershipTier(Tier.Premium);
         return memberRepository.save(member);
     }
@@ -31,8 +50,14 @@ public class MemberServiceImpl implements MemberService {
         if (member.getMembershipTier() == Tier.Regular) {
             throw new RuntimeException("Already cancel to Regular account.");
         }
-        member.setMembershipTier(Tier.Regular);
-        return memberRepository.save(member);
+        try {
+            stripeService.cancelSubscription(member.getSubscriptionId());
+            member.setSubscriptionId(null);
+            member.setMembershipTier(Tier.Regular);
+            return memberRepository.save(member);
+        } catch (RuntimeException e) {
+            throw e;
+        }
     }
 
     public Member addPoint(long id, int point) {
