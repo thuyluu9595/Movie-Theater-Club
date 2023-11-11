73111a4fdde2ad5daeaffe752b94337ca55e55ad
Chau Doan <chaudoan@Chaus-MBP.attlocal.net>
2023-11-10 22:21:03 -0800

    chau

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import React, { useContext } from 'react';
-import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
-import  LinkContainer from 'react-router-bootstrap/LinkContainer';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
 import HomeScreen from './screens/HomeScreen';
 import MovieScreen from './screens/MovieScreen';
 import SigninScreen from './screens/SigninScreen';
@@ -14,20 +14,20 @@ import { LocationScreen } from './screens/LocationScreen';
 import PaymentScreen from './screens/PaymentScreen';
 
 
-export default function App(){
-  const { state, dispatch: ctxDispatch} = useContext(Store);
-  const {userInfo} = state;
+export default function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
 
   const signoutHandler = () => {
-    ctxDispatch({type: 'USER_SIGNOUT'});
+    ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
   }
-  
+
   return (
     <BrowserRouter>
       <div className='d-flex flex-column site-container'>
         <header>
-          <Navbar style={{backgroundColor: '#9d1010', fontFamily: 'Luminari, fantasy', color: 'white'}}>
+          <Navbar style={{ backgroundColor: '#9d1010', fontFamily: 'Luminari, fantasy', color: 'white' }}>
             <Container>
               <LinkContainer to='/'>
                 <Navbar.Brand>THC Theater</Navbar.Brand>
@@ -35,58 +35,68 @@ export default function App(){
               <SearchBox />
               <Nav className='me-auto'>
                 {userInfo ? (
-                  <NavDropdown title={userInfo.role==="Employee" ? 'Admin' : userInfo.first_name}id='basic-nav-dropdown'>
-                    {userInfo.role==="Employee" ? (
-                      <LinkContainer to='/locations'>
-                        <NavDropdown.Item>Location</NavDropdown.Item>
-                      </LinkContainer>
+                  <NavDropdown title={userInfo.role === "Employee" ? 'Admin' : userInfo.first_name} id='basic-nav-dropdown'>
+                    {userInfo.role === "Employee" ? (
+                      <div>
+                        <LinkContainer to='/locations'>
+                          <NavDropdown.Item>Location</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/'>
+                          <NavDropdown.Item>Movies</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/showtimes'>
+                          <NavDropdown.Item>Showtime</NavDropdown.Item>
+                        </LinkContainer>
+                      </div>
+
                     ) : (
-                      <LinkContainer to='/profile'>
-                        <NavDropdown.Item>User Profile</NavDropdown.Item>
-                      </LinkContainer>
+                      <div>
+                        git <LinkContainer to='/profile'>
+                          <NavDropdown.Item>User Profile</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/history'>
+                          <NavDropdown.Item>History</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/premium'>
+                          <NavDropdown.Item>Premium</NavDropdown.Item>
+                        </LinkContainer>
+                      </div>
+
                     )}
-                    {userInfo.role==="Employee" ? (
-                      <LinkContainer to='/'>
-                        <NavDropdown.Item>Movies</NavDropdown.Item>
-                      </LinkContainer>
-                    ) : (
-                      <LinkContainer to='/history'>
-                        <NavDropdown.Item>History</NavDropdown.Item>
-                      </LinkContainer>
-                    )}
+
                     <NavDropdown.Divider />
                     <Link
                       className='dropdown-item'
                       to='#signout'
                       onClick={signoutHandler}>
-                        Sign Out
-                      </Link>
-                  </NavDropdown>
-                  ) : (
-                    <Link className='nav-Link' to='/signin'>
-                      Sign In
+                      Sign Out
                     </Link>
-                  )} 
+                  </NavDropdown>
+                ) : (
+                  <Link className='nav-Link' to='/signin'>
+                    Sign In
+                  </Link>
+                )}
               </Nav>
             </Container>
           </Navbar>
         </header>
         <main>
           <Container className='mt-3'>
-          <Routes>
-            <Route path='/' element={<HomeScreen/>}/>
-            <Route path='/movie/:id' element={<MovieScreen/>}/>
-            <Route path='/signin' element={<SigninScreen/>}/>
-            <Route path='/showtimes' element={<ShowTimeScreen/>}/>
-            <Route path='/register' element={<RegisterScreen/>}/>
-            <Route path='/bookings' element={<BookingScreen/>}/>
-            <Route path='/locations' element={<LocationScreen/>}/>
-            <Route path='/payment' element={<PaymentScreen/>}/>
-          </Routes>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/movie/:id' element={<MovieScreen />} />
+              <Route path='/signin' element={<SigninScreen />} />
+              <Route path='/showtimes' element={<ShowTimeScreen />} />
+              <Route path='/register' element={<RegisterScreen />} />
+              <Route path='/bookings' element={<BookingScreen />} />
+              <Route path='/locations' element={<LocationScreen />} />
+              <Route path='/payment' element={<PaymentScreen />} />
+            </Routes>
           </Container>
         </main>
-        <footer> 
-          <Navbar style={{backgroundColor: '#9d1010', fontFamily: 'Luminari, fantasy', color: 'white'}}>
+        <footer>
+          <Navbar style={{ backgroundColor: '#9d1010', fontFamily: 'Luminari, fantasy', color: 'white' }}>
             <Container className='d-flex justify-content-center align-items-center flex-grow-1'>
               <div className='text-center'>
                 2003-2004 TCH Theater , Inc - All Right Reserver
